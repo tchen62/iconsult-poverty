41c5a452a3242d61331ab730ab4ee8a0044a151d
Chaitanya Chaphalkar <6058414+chaitanya41nexus@users.noreply.github.com>
2019-12-18 10:45:53 +0530

    Initial findings.
    Initial finding are put into comments below the code. Need to drill down on some variables.

--- Merged 10-09.R
+++ Merged 10-09.R
@@ -162,7 +162,7 @@ for (i in 4:length(test)){
   corr <- c(corr, a)
 }
 correlation_tracts <- na.omit(data.frame(census_tract,corr))
-correlation_tracts <- correlation_tracts[with(corr_table, order(-corr)),]
+correlation_tracts <- correlation_tracts[with(correlation_tracts, order(-corr)),]
 correlation_tracts$label <- variables$label[match(correlation_tracts$census_tract,variables$name)]
 correlation_tracts$concept<-variables$concept[match(correlation_tracts$census_tract,variables$name)]
 write.csv(correlation_tracts, 'correlation_tracts_labeled_sorted.csv')
@@ -170,3 +170,28 @@ write.csv(correlation_tracts, 'correlation_tracts_labeled_sorted.csv')
 head(correlation_tracts,50)
 #Last 50 corr (Negative)
 tail(correlation_tracts,50)
+
+#For positive correlations
+#People below poverty level also seem to have some kind of disability(auditory, vision, cognitive, ambulatory, self-care). This could be one area for City of syracuse to focus on.
+#Auditory(hearing impairment) 97%, vision 97%, cognitive 93%, ambulatory(walking) 93%, self-care disability 93%.
+#There are also age based disabilities in the same categories that come up in negative correlation implying that young people who have disabilities dont contribute to poverty as much
+#as older people.
+#Their poverty level may be helped by providing disability specific aids to them. correlation ranges from 97% to 93% suggesting most poor people have some form of disability.
+#People below poverty level also seem to have a veteran status. correlation is 85% suggesting if a person is veteran, there is an 85% chance that they are below poverty level.
+#Housing costs with high tenure also seem to affect people below poverty level, Correlation is 61.9%. This can be helped by subsidized housing plans for people below poverty level.
+#Other psosible areas of focus: Class of worker, marital status, school enrollment(college or graduate), worker experience(by income and earnings). 
+#These areas need to be drilled down in detail to find out which specific type affects poverty level the most.
+#
+#For negative correlations
+#Health insurance has a negative role to play. 97% People below poverty line do not haveaccess to health insurance(private or otherwise).
+#98.5% people below pverty level seem to have negtive correlation with school enrollment(graduate or college).
+#Income has a negative correlation of 70% suggesting that income although is a factor, it is still affected by all of the other factors discussed above.
+#56% of the people below pverty level have not used food stamps or may not have access to them.
+# 55% Females below pverty level from 6-18 years do not have health insurance coverage.
+# 59% people below poverty level who are employed by for-profit companies have not received their salaries or wages.
+#64% females have none to negligible aggregate work hours suggesting they do not earn or work resulting in poverty.
+#66% females workers below pverty level work within resident state and the other work out of state of residence.
+#85% female veterans are also belo poverty level.
+
+
+#geographical mobility suggests that 99% people below pverty level are unable to travel to or settle in a different state than their origin.
